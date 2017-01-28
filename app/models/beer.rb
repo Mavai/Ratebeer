2c0e0b201a64de7c8aae28fcb681f522d3df3731
Mavai <marko.vainio14@gmail.com>
2017-01-28 18:26:24 +0200

    week 2 submission

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,8 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings, dependent: :destroy
+
+  def to_s
+    "#{name} - #{brewery.name}"
+  end
 end
---
+++ app/models/concerns/rating_average.rb
@@ -0,0 +1,8 @@
+module RatingAverage
+    extend ActiveSupport::Concern
+    
+    def average_rating
+        ratings.average(:score).round(1)
+    end
+
+end
